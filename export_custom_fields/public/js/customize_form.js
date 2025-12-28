0b2d350fe1b8594c4c1b077facdb7ec55563b9b9
abdopcnet <abdopcnet@gmail.com>
2025-12-28 13:24:34 +0000

    🔄 customize_form.js

--- export_custom_fields/public/js/customize_form.js
+++ export_custom_fields/public/js/customize_form.js
@@ -1,55 +1,51 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // MIT License. See license.txt
 
-frappe.provide("frappe.customize_form");
+frappe.provide('frappe.customize_form');
 
-frappe.ui.form.on("Customize Form", {
+frappe.ui.form.on('Customize Form', {
 	refresh: function (frm) {
-		if (frappe.boot.developer_mode) {
-			// Export to Module button
-			frm
-				.add_custom_button(
-					__("📦 Export to Module"),
-					function () {
-						frappe.prompt(
-							[
-								{
-									fieldtype: "Link",
-									fieldname: "module",
-									options: "Module Def",
-									label: __("Module to Export"),
-									reqd: 1,
-								},
-								{
-									fieldtype: "Check",
-									fieldname: "sync_on_migrate",
-									label: __("Sync on Migrate"),
-									default: 1,
-								},
-							],
-							function (data) {
-								frappe.call({
-									method: "export_custom_fields.customize_form.export_custom_fields_by_module",
-									args: {
-										module: data.module,
-										sync_on_migrate: data.sync_on_migrate,
-									},
-									callback: function (r) {
-										if (!r.exc) {
-											frappe.show_alert({
-												message: __("Custom Fields exported successfully"),
-												indicator: "green",
-											});
-										}
-									},
-								});
+		if (frappe.boot.developer_mode && frm.doc.doc_type) {
+			frm.add_custom_button(__('Export Customizations'), function () {
+				frappe.prompt(
+					[
+						{
+							fieldtype: 'Link',
+							fieldname: 'module',
+							options: 'Module Def',
+							label: __('Module to Export'),
+							reqd: 1,
+						},
+						{
+							fieldtype: 'Check',
+							fieldname: 'sync_on_migrate',
+							label: __('Sync on Migrate'),
+							default: 1,
+						},
+						{
+							fieldtype: 'Check',
+							fieldname: 'with_permissions',
+							label: __('Export Custom Permissions'),
+							description: __(
+								'Exported permissions will be force-synced on every migrate overriding any other customization.',
+							),
+							default: 0,
+						},
+					],
+					function (data) {
+						frappe.call({
+							method: 'frappe.modules.utils.export_customizations',
+							args: {
+								doctype: frm.doc.doc_type,
+								module: data.module,
+								sync_on_migrate: data.sync_on_migrate,
+								with_permissions: data.with_permissions,
 							},
-							__("Select Module")
-						);
-					}
-				)
-				.addClass("btn-danger");
+						});
+					},
+					__('Select Module'),
+				);
+			}).addClass('btn-danger');
 		}
 	},
 });
-
