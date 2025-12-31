15f836f50d86612b9d4b5fca258f51814c02881f
abdopcnet <abdopcnet@gmail.com>
2025-12-31 21:01:05 +0000

    🔄 property_setter.js

--- export_custom_fields/public/js/property_setter.js
+++ export_custom_fields/public/js/property_setter.js
@@ -10,47 +10,47 @@ frappe.ui.form.on('Property Setter', {
 		if (frappe.boot.developer_mode) {
 			// Export Customizations button
 			if (frm.doc.doc_type) {
-				frm.add_custom_button(__('Export Customizations'), function () {
-					frappe.prompt(
-						[
-							{
-								fieldtype: 'Link',
-								fieldname: 'module',
-								options: 'Module Def',
-								label: __('Module to Export'),
-								reqd: 1,
-								default: frm.doc.module || '',
-							},
-							{
-								fieldtype: 'Check',
-								fieldname: 'sync_on_migrate',
-								label: __('Sync on Migrate'),
-								default: 1,
-							},
-							{
-								fieldtype: 'Check',
-								fieldname: 'with_permissions',
-								label: __('Export Custom Permissions'),
-								description: __(
-									'Exported permissions will be force-synced on every migrate overriding any other customization.',
-								),
-								default: 0,
-							},
-						],
-						function (data) {
-							frappe.call({
-								method: 'frappe.modules.utils.export_customizations',
-								args: {
-									doctype: frm.doc.doc_type,
-									module: data.module,
-									sync_on_migrate: data.sync_on_migrate,
-									with_permissions: data.with_permissions,
-								},
-							});
+			frm.add_custom_button(__('Export Customizations'), function () {
+				frappe.prompt(
+					[
+						{
+							fieldtype: 'Link',
+							fieldname: 'module',
+							options: 'Module Def',
+							label: __('Module to Export'),
+							reqd: 1,
+							default: frm.doc.module || '',
 						},
-						__('Select Module'),
-					);
-				}).addClass('btn-danger');
+						{
+							fieldtype: 'Check',
+							fieldname: 'sync_on_migrate',
+							label: __('Sync on Migrate'),
+							default: 1,
+						},
+						{
+							fieldtype: 'Check',
+							fieldname: 'with_permissions',
+							label: __('Export Custom Permissions'),
+							description: __(
+								'Exported permissions will be force-synced on every migrate overriding any other customization.',
+							),
+							default: 0,
+						},
+					],
+					function (data) {
+						frappe.call({
+							method: 'frappe.modules.utils.export_customizations',
+							args: {
+								doctype: frm.doc.doc_type,
+								module: data.module,
+								sync_on_migrate: data.sync_on_migrate,
+								with_permissions: data.with_permissions,
+							},
+						});
+					},
+					__('Select Module'),
+				);
+			}).addClass('btn-danger');
 			}
 
 			// Set Module button
