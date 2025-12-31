67cce886657e88c31b5e4fe4c94206a87c8330e1
abdopcnet <abdopcnet@gmail.com>
2025-12-31 21:01:05 +0000

    🔄 custom_html_block.js

--- export_custom_fields/public/js/custom_html_block.js
+++ export_custom_fields/public/js/custom_html_block.js
@@ -9,45 +9,45 @@ frappe.ui.form.on('Custom HTML Block', {
 			// Export to Module button
 			if (frm.doc.name) {
 				frm.add_custom_button(__('Export to Module'), function () {
-					frappe.prompt(
-						[
-							{
-								fieldtype: 'Link',
-								fieldname: 'module',
-								options: 'Module Def',
-								label: __('Module to Export'),
-								reqd: 1,
+				frappe.prompt(
+					[
+						{
+							fieldtype: 'Link',
+							fieldname: 'module',
+							options: 'Module Def',
+							label: __('Module to Export'),
+							reqd: 1,
 								default: frm.doc.module || '',
-							},
-							{
-								fieldtype: 'Check',
-								fieldname: 'sync_on_migrate',
-								label: __('Sync on Migrate'),
-								default: 1,
-							},
-						],
-						function (data) {
-							frappe.call({
-								method: 'export_custom_fields.custom_html_block.export_custom_html_blocks_by_module',
-								args: {
-									name: frm.doc.name,
-									module: data.module,
-									sync_on_migrate: data.sync_on_migrate,
-								},
-								callback: function (r) {
-									if (!r.exc) {
-										frappe.show_alert({
-											message: __('Custom HTML Block exported successfully'),
-											indicator: 'green',
-										});
-									}
-								},
-							});
 						},
-						__('Select Module'),
-					);
-				}).addClass('btn-danger');
-			}
+						{
+							fieldtype: 'Check',
+							fieldname: 'sync_on_migrate',
+							label: __('Sync on Migrate'),
+							default: 1,
+						},
+					],
+					function (data) {
+						frappe.call({
+							method: 'export_custom_fields.custom_html_block.export_custom_html_blocks_by_module',
+							args: {
+								name: frm.doc.name,
+								module: data.module,
+								sync_on_migrate: data.sync_on_migrate,
+							},
+							callback: function (r) {
+								if (!r.exc) {
+									frappe.show_alert({
+										message: __('Custom HTML Block exported successfully'),
+										indicator: 'green',
+									});
+								}
+							},
+						});
+					},
+					__('Select Module'),
+				);
+			}).addClass('btn-danger');
+		}
 
 			// Set Module button
 			if (frm.doc.name) {
